c78e03668ba616badd5dd9f29c1a904b343d1bcd
Prabin Parajuli <prabin.b9@gmail.com>
2021-04-20 12:03:43 +0200

    Add files via upload

---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>Reaction time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 200) {
+      this.rank = "Prabinhood Achieved";
+    } else if (this.score < 400) {
+      this.rank = "Rapid fire";
+    } else {
+      this.rank = "Snail snail";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4em;
+  color: green;
+  font-weight: bold;
+}
+</style>
